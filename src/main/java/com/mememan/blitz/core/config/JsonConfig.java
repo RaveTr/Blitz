a15ffed27df839a05b52f5bfc37a970ae0c8e546
Meme Man <yuyuss210@gmail.com>
2026-02-22 00:36:26 +0200

    Tweak default config values

--- src/main/java/com/mememan/blitz/core/config/JsonConfig.java
+++ src/main/java/com/mememan/blitz/core/config/JsonConfig.java
@@ -46,7 +46,7 @@ public final class JsonConfig {
     // Config Values
     public static final ConfigValue ENABLE_SCREEN_SHAKE = registerClientConfigValue("enable_screen_shake", new JsonPrimitive(true), "Whether or not screen shake should be enabled (only really present when using the Ore Sonar at the moment).");
 
-    public static final ConfigValue ORE_SONAR_SCAN_RADIUS = registerConfigValue("ore_sonar_scan_radius", new JsonPrimitive(1024), "The radius (in blocks) in which the Ore Sonar should scan for ores.", 10.0D, 2048.0D);
+    public static final ConfigValue ORE_SONAR_SCAN_RADIUS = registerConfigValue("ore_sonar_scan_radius", new JsonPrimitive(32), "The radius (in blocks, relative to scanning pos) in which the Ore Sonar should scan for ores.", 10.0D, 2048.0D);
     public static final ConfigValue ORE_SONAR_SCAN_LIMIT = registerConfigValue("ore_sonar_scan_limit", new JsonPrimitive(512), "The maximum number of ores that the Ore Sonar should scan for.", 10.0D, 1024.0D);
     public static final ConfigValue ORE_SONAR_COOLDOWN = registerConfigValue("ore_sonar_cooldown", new JsonPrimitive(100), "The cooldown (in ticks) for the Ore Sonar.", 20.0D, 10000.0D);
 
